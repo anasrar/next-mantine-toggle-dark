9f5fe481ea7da8e1187200c30b90ead0044d7bb9
anasrar <mynameanasrar@gmail.com>
2022-11-07 16:12:50 +1300

    Initial commit

---
+++ app/emotion.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import { CacheProvider } from '@emotion/react';
+import { useEmotionCache, MantineProvider, ColorScheme, ColorSchemeProvider } from '@mantine/core';
+import { useServerInsertedHTML } from 'next/navigation';
+import { useState } from 'react';
+
+export default function RootStyleRegistry({ children }: { children: React.ReactNode }) {
+  const cache = useEmotionCache();
+  cache.compat = true;
+
+  useServerInsertedHTML(() => (
+    <style
+      data-emotion={`${cache.key} ${Object.keys(cache.inserted).join(' ')}`}
+      dangerouslySetInnerHTML={{
+        __html: Object.values(cache.inserted).join(' '),
+      }}
+    />
+  ));
+
+  const [colorScheme, setColorScheme] = useState<ColorScheme>('light');
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
+
+  return (
+    <CacheProvider value={cache}>
+      <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+        <MantineProvider theme={{ colorScheme }} withGlobalStyles withNormalizeCSS>
+          {children}
+        </MantineProvider>
+      </ColorSchemeProvider>
+    </CacheProvider>
+  );
+}
